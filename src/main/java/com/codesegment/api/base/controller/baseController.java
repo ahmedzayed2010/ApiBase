e0fc713aa1f9627f35d9996f8cfa12ee0bbc2e28
ahmedzayed2010 <ahmedzayed2010@gmail.com>
2023-03-22 09:18:03 +0300

    initial swagger configuration

--- src/main/java/com/codesegment/api/base/controller/baseController.java
+++ src/main/java/com/codesegment/api/base/controller/baseController.java
@@ -1,15 +1,17 @@
 package com.codesegment.api.base.controller;
 
 import com.codesegment.api.base.shared.BaseResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/base")
+@RequestMapping("/api/javainuse")
 public class baseController {
-    @GetMapping("/test")
+   @GetMapping(value="/test" )
     public ResponseEntity<BaseResponse<String>> test() {
         BaseResponse<String> response = new BaseResponse<>();
         response.setData("First Step -- 17-1-2023");
